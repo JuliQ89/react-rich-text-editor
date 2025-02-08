2977e6e67c4e7fe84291f3d7dd1e617e516aa493
JuliQ89 <julian.singheiser.official@gmail.com>
2025-02-08 15:33:08 +0100

    updated

--- dist/RichTextEditor.js
+++ dist/RichTextEditor.js
@@ -5,22 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./assets/css/editor.css");
 const react_1 = require("react");
-const html_react_parser_1 = __importDefault(require("html-react-parser"));
 const configs_1 = require("./configs");
 const react_2 = __importDefault(require("react"));
 const RichTextEditor = ({ editorContent, maxHeight, height, setEditorContent, spellCheck = true, toolbarConfigs = configs_1.toolbarDefaultConfigs, }) => {
-    const [value] = (0, react_1.useState)(editorContent);
     const { toolbar, checkCommandsState } = (0, configs_1.useToolbar)();
+    const editorRef = (0, react_1.useRef)(null);
+    (0, react_1.useEffect)(() => {
+        if (editorRef.current && editorRef.current.innerHTML !== editorContent) {
+            editorRef.current.innerHTML = editorContent;
+        }
+    }, [editorContent]);
     return (react_2.default.createElement("div", { className: "rich-text-container", style: { height } },
         react_2.default.createElement("header", { className: "rich-text-toolbar" }, toolbarConfigs.map((config) => {
             return toolbar[config];
         })),
         react_2.default.createElement("main", { className: "rich-text-main", style: { maxHeight } },
-            react_2.default.createElement("div", { id: "rich-text-editor", className: "rich-text-field", contentEditable: true, spellCheck: spellCheck, onMouseUp: checkCommandsState, onMouseDown: checkCommandsState, onInput: (e) => {
+            react_2.default.createElement("div", { id: "rich-text-editor", className: "rich-text-field", ref: editorRef, contentEditable: true, spellCheck: spellCheck, onMouseUp: checkCommandsState, onMouseDown: checkCommandsState, onInput: (e) => {
                     const target = e.target;
                     setEditorContent(target.innerHTML);
                     checkCommandsState();
-                }, suppressContentEditableWarning: true }, (0, html_react_parser_1.default)(value)),
+                }, suppressContentEditableWarning: true }),
             react_2.default.createElement("input", { type: "hidden", name: "content", value: editorContent, id: "rich-text-input" }))));
 };
 exports.default = RichTextEditor;
